e166ff096e47e5395cb9e84c7ee29732f460541a
k1llowatt <guryanov.m.a@gmail.com>
2022-02-11 17:04:35 +0300

    Connected DB to controller and implemented simple CRUD in NestJs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 import { TodoController } from './todo/controllers/todo.controller';
+import { TodoService } from './todo/services/todo.service';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), TodoModule],
-  controllers: [TodoController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/todo/controllers/todo.controller.ts
+++ src/todo/controllers/todo.controller.ts
@@ -3,40 +3,69 @@ import {
   Controller,
   Delete,
   Get,
+  HttpException,
+  HttpStatus,
   Param,
   Post,
   Put,
 } from '@nestjs/common';
-import { Todo } from '../entities/todo.entity';
 import { CreateDto, UpdateDto } from './dto';
+import { TodoService } from '../services/todo.service';
+import { Todo } from '../entities/todo.entity';
 
 @Controller('/rest/todo')
 export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
   @Get()
-  getAction(): string {
-    return 'GEt All';
+  getAction(): Promise<Todo[]> {
+    return this.todoService.findAll();
   }
 
   @Get(':id')
-  getOneAction(@Param('id') id: string) {
-    return `Get one = ${id}`;
+  async getOneAction(@Param('id') id: string): Promise<Todo> {
+    const todo = this.todoService.findOne(id);
+    if (todo === undefined) {
+      console.log('Exception');
+      throw new HttpException('Exception', HttpStatus.NOT_FOUND);
+    }
+    console.log(todo);
+    return todo;
   }
 
   @Post()
-  createAction(@Body() todo: CreateDto): CreateDto {
-    console.log(todo);
-    return todo;
+  createAction(@Body() createDto: CreateDto): Promise<Todo> {
+    const todo = new Todo();
+    todo.title = createDto.title;
+    if (todo.isCompleted !== undefined) {
+      todo.isCompleted = createDto.isCompleted;
+    }
+    console.log(`post ` + todo.title);
+    return this.todoService.create(todo);
   }
 
   @Put(':id')
-  updateAction(@Param('id') id: string, @Body() todo: UpdateDto): UpdateDto {
-    console.log('Search by ID ', todo);
+  async updateAction(
+    @Param('id') id: string,
+    @Body() { title, isCompleted = false }: UpdateDto,
+  ): Promise<Todo> {
+    const todo = await this.todoService.findOne(id);
     console.log(todo);
-    return todo;
+    if (todo === undefined) {
+      console.log('Exception');
+      throw new HttpException('Exception', HttpStatus.NOT_FOUND);
+    }
+    todo.title = title;
+    todo.isCompleted = isCompleted;
+    return this.todoService.update(todo);
   }
 
   @Delete(':id')
-  deleteAction(): string {
-    return 'delete';
+  deleteAction(@Param('id') id: string): Promise<void> {
+    return this.todoService.remove(id);
   }
+  /*  @Delete()
+  deleteAllAction(@Param('id') id: string): Promise<void> {
+    return this.todoService.remove();
+  }*/
 }
